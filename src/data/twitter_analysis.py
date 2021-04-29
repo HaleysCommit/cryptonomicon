4bba333e7702ed0ed3124eb5cd61669e86898de6
Marshall Felder <marshall.felder@gmail.com>
2021-04-29 04:31:33 -0500

    Begin follower analysis using Safemoon as an example

--- src/data/twitter_analysis.py
+++ src/data/twitter_analysis.py
@@ -11,15 +11,20 @@ bearer_token = 'AAAAAAAAAAAAAAAAAAAAANUyOwEAAAAAQ%2BG3hHsiUozETzs6gfPT2%2BpKECM%
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, secret_token)
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
 # the ID of the user - use https://commentpicker.com/twitter-id.php
 id = 1366256262921523205
   
 # fetching the user
 user = api.get_user(id)
-  
+follower_info = api.followers(user, -1)
 # fetching the followers_count
 followers_count = user.followers_count
+username = user.name
   
-print("The number of followers of the user are : " + str(followers_count))
+print(f'{username} has {followers_count} followers'.format(username, followers_count))
+
+for follower in api.followers(username):
+    if follower.followers_count > 1000:
+        print(f'{follower.name} has {follower.followers_count} followers'.format(follower.name, follower.followers_count))
